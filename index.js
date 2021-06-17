fc272122f91fcc1e0b2c19b8a761ec23e2b595bc
Muhammad Ahsan Sabir <55804800+Ahsan2001@users.noreply.github.com>
2021-06-17 23:23:58 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ var current = new Date();
 
 // Get New Time
 var time = current.toLocateTimeString();
+
+
+// To Replace String Character
+
+const = "My name is Ahsan"
+
+console.log (p.repalce('Ahsan' , 'Ehsan'));
